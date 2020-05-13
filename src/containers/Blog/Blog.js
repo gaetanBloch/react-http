7dde201f6b999713ab576eb391e2ad9c93aeb64a
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-13 09:22:23 +0200

    Handling Requests Errors Locally with promise catch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
   state = {
     posts: [],
-    selectedPostId: null
+    selectedPostId: null,
+    error: false
   }
 
   getPosts = async () => {
@@ -25,19 +26,22 @@ class Blog extends Component {
   }
 
   componentDidMount() {
-    // axios.get('https://jsonplaceholder.typicode.com/posts')
-    //   .then((response) => {
-    //     const posts = response.data.slice(0, 4);
-    //     const updatedPosts = posts.map(post => {
-    //       return {
-    //         ...post,
-    //         author: 'Gaëtan'
-    //       }
-    //     })
-    //     this.setState({posts: updatedPosts})
-    //   })
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Gaëtan'
+          }
+        })
+        this.setState({posts: updatedPosts})
+      })
+      .catch(error => {
+        this.setState({error: true});
+      })
 
-    this.getPosts();
+    // this.getPosts();
   }
 
   postSelectedHandler = (id) => {
@@ -45,13 +49,16 @@ class Blog extends Component {
   }
 
   render() {
-    const posts = this.state.posts.map(post => {
-      return <Post
-        key={post.id}
-        title={post.title}
-        author={post.author}
-        clicked={() => this.postSelectedHandler(post.id)} />;
-    });
+    let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)} />;
+      });
+    }
 
     return (
       <div>
