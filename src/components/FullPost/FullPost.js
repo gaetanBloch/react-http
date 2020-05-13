13b066db9b699e98693b20d924ba9506c670b336
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-13 08:59:49 +0200

    Sending a DELETE data to server

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -27,6 +27,20 @@ class FullPost extends Component {
     }
   }
 
+  deletePost = async () => {
+    const response = await axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`);
+    console.log(response);
+  }
+
+  deletePostHandler = () => {
+    // axios.delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+    //   .then(response => {
+    //     console.log(response);
+    //   })
+    this.deletePost();
+  }
+
   render() {
     let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>
 
@@ -40,7 +54,7 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
           </div>
         </div>
       );
